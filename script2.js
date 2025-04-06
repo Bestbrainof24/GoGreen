8880775e09a61542fcb4fd2c576bd287163522a4
Bestbrainof25 <asserelgendy2011@gmail.com>
2025-04-06 12:53:24 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,9 @@
 let name = sessionStorage.getItem('name');
 
 if (name) {
-  document.getElementById('greeting').textContent = `Welcome, ${name}!`;
+  document.getElementById('greeting').textContent = `Hello, ${name}!`;
 } else {
-  document.getElementById('greeting').textContent = 'Welcome, Guest!';
+  document.getElementById('greeting').textContent = 'Hello, Guest!';
 }
 
 setTimeout(() => {
